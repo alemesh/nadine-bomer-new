65daf59b9a644f952b0cdd16624185ec66a5699f
alemesh <alemesh@acceptic.com>
2018-05-17 20:06:27 +0300

    wre

--- app/js/common.js
+++ app/js/common.js
@@ -43,3 +43,26 @@ $(".custom-option").on("click", function() {
     $(this).parents(".custom-select").find(".custom-select-trigger").text($(this).text());
 });
 //==========================================
+
+//=============================scroll on form=============================
+$(function() {
+
+    $(document).ready(function(){
+        $(".button-mobile").on("click","a", function (event) {
+            //отменяем стандартную обработку нажатия по ссылке
+            event.preventDefault();
+
+            //забираем идентификатор бока с атрибута href
+            var id  = $(this).attr('href'),
+
+                //узнаем высоту от начала страницы до блока на который ссылается якорь
+                top = $(id).offset().top;
+
+            //анимируем переход на расстояние - top за 1500 мс
+            $('body,html').animate({scrollTop: top}, 500);
+        });
+    });
+
+});
+
+//====================================================================
